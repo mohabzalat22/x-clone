13b67cf8a73b86252a7ed248ed8da8f68bec4c9a
mohabzalat22 <mohabzalat22@gmail.com>
2024-05-24 23:40:20 +0300

    new registeration page

---
+++ src/pages/RegisterPage.vue
@@ -0,0 +1,27 @@
+<script>
+</script>
+<template>
+<div class="">
+    <div class="max-w-md border p-6 rounded mx-auto mt-12 bg-gray-50">
+        <h1 class="text-2xl font-bold my-4 text-gray-800">Register your account</h1>
+        <form class="space-y-4 md:space-y-6" action="#">
+            <div>
+                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 ">name</label>
+                <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name">
+            </div>
+            <div>
+                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
+                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="email@company.com">
+            </div>
+            <div>
+                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
+            </div>
+ 
+            <button type="submit" class="w-full text-white bg-blue-400 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign up</button>
+            
+        </form>
+    </div>
+
+</div>
+</template>
