69652212bb0c0d2ad967182c845bc5a5952e67c9
mohabzalat22 <mohabzalat22@gmail.com>
2024-05-24 23:26:19 +0300

    updating router for adding login and register pages.

--- src/router/index.js
+++ src/router/index.js
@@ -3,33 +3,66 @@ import HomePage from '../pages/HomePage.vue';
 import CategoryPage from '../pages/CategoryPage.vue';
 import DetailPage from '../pages/DetailPage.vue';
 import PaymentPage from '../pages/PaymentPage.vue';
+import LoginPage from '../pages/LoginPage.vue';
+import RegisterPage from '../pages/RegisterPage.vue';
+
+
 
 // nested routes
 import BillinginfoView from '@/components/payment/BillinginfoView.vue';
 import RentalinfoView from '@/components/payment/RentalinfoView.vue';
 import PaymentMethodView from '@/components/payment/PaymentMethodView.vue';
 import ConfirmationView from '@/components/payment/ConfirmationView.vue';
+import NavbarView from '@/components/NavbarView.vue';
+import FooterView from '@/components/FooterView.vue';
 
 const routes = [
     {
+        path: '/login',
+        name: LoginPage,
+        component: LoginPage,
+    },
+    {
+        path: '/register',
+        name: RegisterPage,
+        component: RegisterPage,
+    },
+    {
         path: '/',
         name: HomePage,
-        component: HomePage,
+        components: 
+        {
+            default: HomePage,
+            NavbarView,
+            FooterView
+        },
     },
     {
         path: '/category',
         name: CategoryPage,
-        component: CategoryPage,
+        components: {
+            default: CategoryPage,
+            NavbarView,
+            FooterView
+        },
     },
     {
         path: '/detail/:id',
         name: DetailPage,
-        component: DetailPage,
+        components: {
+            default: DetailPage,
+            NavbarView,
+            FooterView
+        },
     },
     {
         path: '/payment/:id',
         name: PaymentPage,
-        component: PaymentPage,
+        components: {
+            default: PaymentPage,
+            NavbarView,
+            FooterView
+        },
         children:[
             { 
                 path: '',
