ff23bc597f06ad30e70b567c52c80e765e3fb3ae
mohabzalat22 <mohabzalat22@gmail.com>
2024-06-05 21:06:29 +0300

    check auth user status then login.

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,27 +1,46 @@
 <script setup>
 import axios from 'axios';
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
 axios.defaults.baseURL = 'http://localhost:8000';
 axios.defaults.withXSRFToken = true;
 axios.defaults.withCredentials = true;
+const router = useRouter();
 
 const email = ref(null);
 const password = ref(null);
-const data = ref(null);
 
 const login = async() => {
-    const res = await axios.post('/api/login', {
-        email: email.value,
-        password: password.value
+    const user = await axios.get('/api/user', {  
+        validateStatus: function (status) {
+        // Allow all status codes
+            if(status == 200 || status == 401){
+                return true;
+            }
+        }
     });
-    data.value = res.data;
+    if(user.status == 401){        
+        const res = await axios.post('/api/login', {
+            email: email.value,
+            password: password.value
+        });
+        if(res.status == 200){ //logged in
+            router.push('/profile')
+        }
+    }
+    else{
+        console.log('already autheticated');
+        router.push('/profile')
+    }   
 }
+
 </script>
 <template>
 <div class="">
     <div class="max-w-md border p-6 rounded mx-auto mt-12 bg-gray-50">
         <h1 class="text-2xl font-bold my-4 text-gray-800">Sign in your account</h1>
-        <form class="space-y-4 md:space-y-6" action="http://localhost:8000/login" >
+        <form class="space-y-4 md:space-y-6">
             <div>
                 <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
                 <input v-model="email" type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="email@company.com">
